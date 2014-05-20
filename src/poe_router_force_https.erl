b57b75fe2892e1b9101b8e5c118726e08d747051
camshaft <bytheway.cameron@gmail.com>
2014-05-20 16:33:36 -0600

    force ssl if flag set

--- src/poe_router_force_https.erl
+++ src/poe_router_force_https.erl
@@ -3,5 +3,16 @@
 -export([execute/2]).
 
 execute(Req, Env) ->
-  %% TODO
-  {ok, Req, Env}.
+  case fast_key:get(force_https, Env) of
+    true ->
+      case cowboy_req:header(<<"x-forwarded-proto">>, Req) of
+        {<<"http">>, Req2} ->
+          {<<"http://", Rest/binary>>, Req3} = cowboy_req:url(Req2),
+          {ok, Req4} = cowboy_req:reply(302, [{<<"location">>, <<"https://", Rest/binary>>}], Req3),
+          {halt, Req4};
+        {_, Req2} ->
+          {ok, Req2, Env}
+      end;
+    _ ->
+      {ok, Req, Env}
+  end.
